206a7f6608a5791f84e178f38fef813514369d28
dimawko <sagyntkan@gmail.com>
2022-03-16 17:31:51 +0600

    Refactor for better readability

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -21,6 +21,7 @@ extension ResultViewController {
     private func getResult() {
         let animalValues = answersChosen.reduce(into: [:], { $0[$1, default: 0] += 1 })
         guard let animalResult = animalValues.max(by: { $0.value < $1.value }) else { return }
+        
         animalResultLabel.text = "Вы - \(animalResult.key.animal.rawValue)"
         animalDefinitionLabel.text = animalResult.key.animal.definition
     }
@@ -29,6 +30,7 @@ extension ResultViewController {
     private func getResultSecondExample() {
         let animalCountedSet = NSCountedSet(array: answersChosen)
         guard let animalResult = answersChosen.max(by: { animalCountedSet.count(for: $0) < animalCountedSet.count(for: $1) }) else { return }
+        
         animalResultLabel.text = "Вы - \(animalResult.animal.rawValue)"
         animalDefinitionLabel.text = animalResult.animal.definition
     }
