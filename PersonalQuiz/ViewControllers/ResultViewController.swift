7c4e76146499bf4cf043d30c685e573d8d33ad5c
dimawko <sagyntkan@gmail.com>
2022-03-16 17:15:07 +0600

    Refactor using reduce(into: ) method instead NSCountedSet for better readability

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -12,15 +12,13 @@ class ResultViewController: UIViewController {
         super.viewDidLoad()
         
         getResult()
-        
         navigationItem.hidesBackButton = true
     }
     
     private func getResult() {
-        let animalCountedSet = NSCountedSet(array: answersChosen)
-        guard let animalResult = answersChosen.max(by: { animalCountedSet.count(for: $0) < animalCountedSet.count(for: $1) }) else { return }
-        animalResultLabel.text = "Вы - \(animalResult.animal.rawValue)"
-        animalDefinitionLabel.text = animalResult.animal.definition
+        let animalValues = answersChosen.reduce(into: [:], { $0[$1, default: 0] += 1 })
+        guard let animalResult = animalValues.max(by: { $0.value < $1.value }) else { return }
+        animalResultLabel.text = "Вы - \(animalResult.key.animal.rawValue)"
+        animalDefinitionLabel.text = animalResult.key.animal.definition
     }
 }
-
