b18672cdeecf013b58b92f142596ee26d747ac79
dimawko <sagyntkan@gmail.com>
2022-03-16 17:31:13 +0600

    Refactor for better readability

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -14,7 +14,10 @@ class ResultViewController: UIViewController {
         getResult()
         navigationItem.hidesBackButton = true
     }
-    
+}
+
+//MARK: - Private methods
+extension ResultViewController {
     private func getResult() {
         let animalValues = answersChosen.reduce(into: [:], { $0[$1, default: 0] += 1 })
         guard let animalResult = animalValues.max(by: { $0.value < $1.value }) else { return }
@@ -23,7 +26,7 @@ class ResultViewController: UIViewController {
     }
     
     //Second implementation example
-    private func getResultExampleTwo() {
+    private func getResultSecondExample() {
         let animalCountedSet = NSCountedSet(array: answersChosen)
         guard let animalResult = answersChosen.max(by: { animalCountedSet.count(for: $0) < animalCountedSet.count(for: $1) }) else { return }
         animalResultLabel.text = "Вы - \(animalResult.animal.rawValue)"
